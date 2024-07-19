9b712c975768476765341d70e5bac9f34b7dd709
Summer Davis <summerd@tlgcohort.com>
2024-07-19 16:21:51 -0500

    api fetching data successfully / category buttons now working properly

--- js-modules/create-gif.js
+++ js-modules/create-gif.js
@@ -2,7 +2,7 @@ export function createGif(gif) {
   let newGif = document.createElement("div");
   newGif.className = "gif";
   newGif.innerHTML = `
-      <img class="gif-img" src=${gif.gif.url} alt=${gif.gif.alt_text}/>
+      <img class="gif-img" src=${gif.images.original.url} alt=${gif.alt_text}/>
       `;
   return newGif;
 }
